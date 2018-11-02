a904429ea4298ed91b3144f64f96b9e934841e4f
nazmulhyder <nhyder.cse131.uiu@gmail.com>
2018-11-03 00:52:45 +0600

    version-1.0,bootstrap 3.3.7

--- Anonymous-311018/Models/Department.cs
+++ Anonymous-311018/Models/Department.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel;
 using System.Linq;
 using System.Web;
 
@@ -9,7 +10,9 @@ namespace Anonymous_311018.Models
     {
         public int Id { get; set; }
         public string Name { get; set; }
+        [DisplayName("Number Of Student")]
         public int NumberofStudent { get; set; }
+        [DisplayName("Number Of Faculty")]
         public int NumberofFaculty { get; set; }
         public ICollection<Student> Students { get; set; }
         public ICollection<Faculty> Faculties { get; set; }
